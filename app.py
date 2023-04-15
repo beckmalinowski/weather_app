ba99c983b1354f61155e3c207e9981b3a798a6ad
beckmalinowski <128921286+beckmalinowski@users.noreply.github.com>
2023-04-15 17:04:36 -0500

    changed from customtkinter to ttkbootstrap

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as ttk
+from PIL import Image, ImageTk
 import requests, bs4
-from PIL import Image
-import shutil
 import datetime
+import shutil
+
+
+H1 = 30
+H2 = 26
+H3 = 20
+H4 = 18
+H5 = 13
+H6 = 10
 
 
 class Weather:
@@ -23,10 +33,8 @@ class Weather:
 
     def get_image(self):
         html_image = self._soup.find("img", class_ = "pull-left")
-
         image_source = html_image.attrs["src"]
         full_image_url = f"https://forecast.weather.gov/{image_source}"
-
         image_root = requests.get(full_image_url, stream = True)
 
         if image_root.status_code == 200:
@@ -35,66 +43,76 @@ class Weather:
               shutil.copyfileobj(image_root.raw, f)
 
 
-class App(ctk.CTk):
-    """customtkinter app"""
+class App(ttk.Window):
+    """tkinter app"""
     def __init__(self, weather: Weather):
         # window setup
-        super().__init__()
+        super().__init__(themename = "vapor")
         self.title("Weather App")
         self.geometry("400x250")
         self.minsize(400, 250)
         self.maxsize(400, 250)
-        # self.iconbitmap("") TODO ico
+        # self.iconbitmap("") TODO
+
+        self._data = weather
+        
+        # will pull the current image associated with the weather conditions
+        # into the assets folder.
+        self._data.get_image()
+        weather_photo = ImageTk.PhotoImage(
+                Image.open("assets/current_image.jpg")
+        )
 
         # TODO set appearance based on time of day
 
-        self._data = weather
-        self._data.get_image()
-        weather_image = ctk.CTkImage(
-                Image.open("assets/current_image.jpg"),
-                size = (100, 100)
-        )
         
-        # layout config
-        title_frame = ctk.CTkFrame(
+        # frame config
+        header = ttk.Frame(self, width = 400, height = 30)
+        image_frame = ttk.Frame(self, width = 100, height = 190)
+        content_frame = ttk.Frame(self, width = 300, height = 190)
+        footer = ttk.Frame(self, width = 400, height = 30)
+
+
+        # header widgets
+        ttk.Label(
+                header,
+                text = "Lawrence, KS",
+                font = ("", H1)
+        ).place(anchor = "center", relx = 0.5, rely = 0.5)
+
+        header.pack(expand = True, fill = "both")
+        
+
+        # weather_photo
+        ttk.Label(
+                image_frame,
+                image = weather_photo,
+                text = ""
+        ).pack(side = "left", expand = True, fill = "x")
+        image_frame.pack(side = "left")
+
+
+        # content_frame widgets
+        # weather description
+        ttk.Label(
                 self,
-                width = 400,
-                height = 30,
-                fg_color = ("#87CEEB", "#0c1445")
-        )
-
-        content_frame = ctk.CTkFrame(
-                self,
-                width = 400,
-                height = 220,
-                fg_color = ("#808080", "#5A5A5A")
-        )
-
-        # other widget config
-        self._Label(
-                title_frame,
-                "Lawrence, KS"
-        ).place(relx = 0.5, rely = 0.5, anchor = "center")
-
-        ctk.CTkLabel(
-                content_frame,
-                text = "",
-                image = weather_image
+                text = self._data.get_condition(),
+                font = ("", H4)
         ).pack()
-        
-        # pack
-        title_frame.pack(expand = True, fill = "both")
+
+        # fahrenheit temperature
+        ttk.Label(
+                self,
+                text = self._data.get_temperature_f()
+        ).pack()
+
         content_frame.pack(expand = True, fill = "both")
+
+
+        # footer widgets
+
+        
+        footer.pack(expand = True, fill = "both")
 
         # run
         self.mainloop()
-
-
-    class _Label(ctk.CTkLabel):
-            def __init__(self, parent, text):
-                super().__init__(
-                        parent,
-                        text = text,
-                        font = ("Calibri", 24),
-                        text_color = ("black", "white"),
-                )
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import requests, bs4
-from app import Weather
-from app import App
+import requests
+from app import Weather, App
 
 
 def main():
+    # hardcoded to use the weather for Lawrence, KS. May implement
+    # a way to search using selenium later
     root = requests.get("https://forecast.weather.gov/MapClick.php?lat=38.973070000000064&lon=-95.23617999999999#.ZDq_6OzMIQ8")
     root.raise_for_status()
     weather_data = Weather(root)
