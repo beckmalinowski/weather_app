c95d60d077a65386a9d21180bb3645fab9fbfc57
beckmalinowski <128921286+beckmalinowski@users.noreply.github.com>
2023-04-15 12:31:49 -0500

    created Weather class to manage weather data for App class.

--- app.py
+++ app.py
@@ -1,31 +1,63 @@
 import customtkinter as ctk
-import requests
-import bs4
+import requests, bs4
+import datetime
+
+
+class Weather:
+    """manages weather data for App instance."""
+    def __init__(self, browser: requests.models.Response):
+        self._soup = bs4.BeautifulSoup(browser.text, "html.parser")
+       
+
+    def get_temperature_f(self):
+        html_temperature = self._soup.select(".myforecast-current-lrg")
+        return html_temperature[0].get_text()
+    
+
+    def get_condition(self):
+        html_condition = self._soup.select(".myforecast-current")
+        return html_condition[0].get_text()
+    
+
+    def get_current_image(self):
+        pass
 
 
 class App(ctk.CTk):
-    def __init__(self, weather: requests.models.Response):
+    """customtkinter app"""
+    def __init__(self, weather: Weather):
         # window setup
         super().__init__()
         self.title("Weather App")
         self.geometry("400x250")
         self.minsize(400, 250)
         self.maxsize(400, 250)
-        
-        self.weather = weather
 
+        self._data = weather
+        
         # widget config
-        ctk.CTkFrame(
+        title_frame = ctk.CTkFrame(
                 self,
                 width = 400,
-                height = 150,
-                fg_color = "purple"
+                height = 50,
+                fg_color = "#87CEEB"
+        )
+
+        self._Label(
+                title_frame,
+                "Lawrence, KS"
         ).pack()
 
+        title_frame.pack(expand = True, fill = "both")
 
         # run
         self.mainloop()
 
 
-    def get_weather_data(self):
-        pass
+    class _Label(ctk.CTkLabel):
+            def __init__(self, parent, text):
+                super().__init__(
+                        parent,
+                        text = text,
+                        font = ("Calibri", 24),
+                )
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import requests, bs4
+from app import Weather
 from app import App
 
 
 def main():
-    weather = requests.get("https://forecast.weather.gov/MapClick.php?lat=38.973070000000064&lon=-95.23617999999999#.ZDq_6OzMIQ8")
-    weather.raise_for_status()
+    root = requests.get("https://forecast.weather.gov/MapClick.php?lat=38.973070000000064&lon=-95.23617999999999#.ZDq_6OzMIQ8")
+    root.raise_for_status()
+    weather_data = Weather(root)
+    App(weather_data)
 
-    # soup = bs4.BeautifulSoup(weather.text, "html.parser")
-    # html_temperature = soup.select(".myforecast-current-lrg")
-    # temperature = html_temperature[0].get_text()
-
-    
-
-    # App(weather)
 
 if __name__ == "__main__":
     main()
